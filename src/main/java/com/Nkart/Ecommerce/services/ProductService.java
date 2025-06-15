0df21cdf6f07161b0bc2d9ca1ab0210fde29a81a
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-15 13:59:00 +0530

    addedproduct service interface for service implemention

---
+++ src/main/java/com/Nkart/Ecommerce/services/ProductService.java
@@ -0,0 +1,16 @@
+package com.Nkart.Ecommerce.services;
+
+import com.Nkart.Ecommerce.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    // add products
+    Product addProduct(Product product);
+    // get all product
+    List<Product> getAllProduct();
+    // get product by id
+    Product getProductById(Long id);
+    // delete product
+    void deleteProduct(Long id);
+}
